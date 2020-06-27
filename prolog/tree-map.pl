9af4a9f2941fe90a6eba18a9539a0c88a7572452
Mee-Tree <89823187178@yandex.ru>
2020-06-27 13:57:44 +0300

    fix tabs

--- prolog/tree-map.pl
+++ prolog/tree-map.pl
@@ -6,25 +6,25 @@ node(Key, Value, Priority, Left, Right).
 merge(T, null, T) :- !.
 merge(T, T, null) :- !.
 merge(node(K2, V2, P2, First, R2), node(K1, V1, P1, L1, R1), node(K2, V2, P2, L2, R2)) :-
-	P2 < P1, !, merge(First, node(K1, V1, P1, L1, R1), L2).
+    P2 < P1, !, merge(First, node(K1, V1, P1, L1, R1), L2).
 merge(node(K1, V1, P1, L1, Second), node(K1, V1, P1, L1, R1), node(K2, V2, P2, L2, R2)) :-
-	P2 >= P1, !, merge(Second, R1, node(K2, V2, P2, L2, R2)).
+    P2 >= P1, !, merge(Second, R1, node(K2, V2, P2, L2, R2)).
 
 % split(Node, Key, First, Second)
 split(null, K, null, null) :- !.
 split(node(K, V, P, L, R), X, First, node(K, V, P, L1, R)) :-
-	X < K, !, split(L, X, First, L1).
+    X < K, !, split(L, X, First, L1).
 split(node(K, V, P, L, R), X, node(K, V, P, L, R1), Second) :-
-	X >= K, !, split(R, X, R1, Second).
+    X >= K, !, split(R, X, R1, Second).
 
 % map_remove(Node, Key, Result)
 map_remove(null, X, null) :- !.
 map_remove(node(K, V, P, L, R), K, Result) :-
     merge(Result, L, R), !.
 map_remove(node(K, V, P, L, R), X, node(K, V, P, L1, R)) :-
-	X < K, !, map_remove(L, X, L1).
+    X < K, !, map_remove(L, X, L1).
 map_remove(node(K, V, P, L, R), X, node(K, V, P, L, R1)) :-
-	X > K, !, map_remove(R, X, R1).
+    X > K, !, map_remove(R, X, R1).
 
 % TODO [Надо переписать на спуск]
 % map_put(Node, Key, Value, Result)
@@ -38,9 +38,9 @@ map_put(Node, K, V, Result) :-
 % map_get(Node, Key, Value)
 map_get(node(K, V, P, L, R), K, V) :- !.
 map_get(node(K, V, P, L, R), X, Val) :-
-	X < K, !, map_get(L, X, Val).
+    X < K, !, map_get(L, X, Val).
 map_get(node(K, V, P, L, R), X, Val) :-
-	X > K, !, map_get(R, X, Val).
+    X > K, !, map_get(R, X, Val).
 
 % map_minKey(Node, Key)
 map_minKey(node(K, V, P, null, _), K) :- !.
@@ -55,5 +55,5 @@ map_maxKey(node(K, V, P, _, R), Key) :-
 % map_build(ListMap, TreeMap)
 map_build([], null) :- !.
 map_build([(K, V) | T], Result) :-
-	 map_build(T, Result1),
-	 map_put(Result1, K, V, Result).
+    map_build(T, Result1),
+    map_put(Result1, K, V, Result).
